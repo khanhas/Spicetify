7276606519e5ccd20c407447d6497529da55c403
Nguyen Xuan Khanh <xuankhanh963@gmail.com>
2018-05-31 18:33:40 +0700

    Added retry mechanic
    Should stop finding websocket after 10 tries

--- @Resources/JavascriptInject/spicetifyWebsocket.js
+++ @Resources/JavascriptInject/spicetifyWebsocket.js
@@ -1,10 +1,14 @@
 (function SpicetifyRainmeterWebsocket() {
     var ws;
-    function Init() {
+    function Init(retry = 0) {
+        if (retry > 10) {
+            console.log("Spicetify websocket was shutdown. Please Reload or Restart Spotify to connect again.")
+            return;
+        }
         var url = "ws://127.0.0.1:58932/Spicetify"
         ws = new WebSocket(url);
         ws.onclose = () => {
-            setTimeout(() => Init(), 5000)
+            setTimeout(() => Init(++retry), 5000)
         };
         ws.onmessage = (event) => {
             var command = event.data.toLowerCase()
