d83b612e3f03539be61fcf28ff02b4dc909300a5
khanhas <xuankhanh963@gmail.com>
2018-07-29 12:34:54 +0700

    Adds cleaning script

---
+++ clean.ps1
@@ -0,0 +1,4 @@
+Remove-Item "./@Resources/robocopy_copyapp_log.txt"
+Remove-Item "./@Resources/robocopy_transfer_log.txt"
+Remove-Item "./@Resources/robocopy_transfer_raw_log.txt"
+Remove-Item "./@Resources/console.log"
